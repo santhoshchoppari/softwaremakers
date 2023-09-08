51c3f2b43007b1932bb981b1d11f19571b313c80
pavangctech <ip105233@gmail.com>
2023-09-08 15:02:38 +0530

    Added my newJava file

---
+++ src/main/java/vamsi/maven/com/vamsi/action/Test.java
@@ -0,0 +1,10 @@
+package vamsi.maven.com.vamsi.action;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out .println("Hi BHUPESH");
+	}
+
+}
